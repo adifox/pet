1bc092981342fc9ad303a2aab534deda57e15177
Lukas <lukasdevarga@gmail.com>
2021-08-26 22:29:22 +0200

    viewport meta added

--- components/layout/index.js
+++ components/layout/index.js
@@ -12,6 +12,10 @@ const Layout = ({ children }) => {
     <>
       <Head>
         <title>Petexcellenttreatment</title>
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0'
+        />
         <link rel='icon' href='/favicon.ico' />
         <link
           rel='stylesheet'
