cdb5cda48dc00cd850933b3457737f2deda210f3
Lukas <lukasdevarga@gmail.com>
2021-09-04 20:38:04 +0200

    document js added for html structure and lang attribute

--- components/super-image-banner/index.js
+++ components/super-image-banner/index.js
@@ -5,7 +5,6 @@ import styles from './superImageBanner.module.css'
 import ImageWrapper from '../ui-components/image-wrapper'
 
 const SuperImageBanner = ({ blok }) => {
-  // const { headline, subtitle, teaserImage } = blok
   return (
     <div className={styles.backgroundDiv}>
       <div className={styles.mainWrapper}>
@@ -20,19 +19,6 @@ const SuperImageBanner = ({ blok }) => {
             />
           </div>
         ))}
-        {/* <div className={styles.heroContainer}>
-        <div className={styles.heroContent}>
-          <div className={styles.heroImageContainer}>
-            <ImageWrapper
-              src={teaserImage.filename}
-              alt='petexcellenttreatment'
-              height={200}
-              width={450}
-              priority={true}
-            />
-          </div>
-        </div>
-      </div> */}
       </div>
     </div>
   )
